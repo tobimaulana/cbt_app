7986e074238cee094bdd8ee15836cba15fe45b34
Tobi Maulana <tobimaulana.tm@gmail.com>
2022-12-10 21:28:05 +0700

    Membuat model jawaban.js

---
+++ models/jawaban.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+
+var koneksi = require("../koneksi");
+
+var Jawaban = koneksi.define('jawaban', {
+       id           : { type: Sequelize.STRING, primaryKey:true },
+       soal_id      : Sequelize.STRING,
+       nis          : Sequelize.STRING,
+       jawaban      : Sequelize.STRING,
+       koreksi      : Sequelize.INTEGER,
+       sync_at      : Sequelize.DATE,
+}, {
+       timestamps:true,
+       tableName:"jawaban"
+});
+
+module.exports=Jawaban;
