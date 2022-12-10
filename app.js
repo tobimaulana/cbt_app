1d6eb5bea2fac85b1b4a265c906762013ed5af6c
Tobi Maulana <tobimaulana.tm@gmail.com>
2022-12-10 20:56:54 +0700

    Modifikasi CORS di app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 
+var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var tagihanRouter = require('./routes/tagihan');
@@ -15,6 +16,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
